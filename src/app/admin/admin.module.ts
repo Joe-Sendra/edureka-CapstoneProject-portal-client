8f2eeac19b57af6c28c4ea4942ff47e1ce79e610
Joe-Sendra <joe.sendra@gmail.com>
2019-12-05 15:58:19 -0600

    removed angular material, added bootstrap

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
-import { MatButtonModule } from '@angular/material/button';
-
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { AdminProfileComponent } from './admin-profile/admin-profile.component';
+import { AdminNavComponent } from './admin-dashboard/admin-nav/admin-nav.component';
 
 // For development purposes only
 import { AdminDevComponent } from './admin-dev/admin-dev.component';
@@ -15,10 +14,10 @@ import { AdminDevService } from './admin-dev/admin-dev.service';
   declarations: [
     AdminDashboardComponent,
     AdminProfileComponent,
-    AdminDevComponent
+    AdminDevComponent,
+    AdminNavComponent
   ],
   imports: [
-    MatButtonModule,
     HttpClientModule,
     CommonModule
   ],
