5d23bf7406aa3e89b580d97767554967e5d094bb
Joe-Sendra <joe.sendra@gmail.com>
2019-12-10 18:24:40 -0600

    display notifications array on home

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { NotificationService } from '../notification/notification.service';
+import { NotificationInfo } from '../notification/notification-info.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit, OnDestroy {
+  notifications: NotificationInfo[];
+  notificationsSub: Subscription;
+
+  constructor(private notificationService: NotificationService) {}
+
+  ngOnInit() {
+    this.notificationsSub = this.notificationService.notificationSub
+      .subscribe(notifications => {
+        this.notifications = notifications;
+      });
+  }
+
+  ngOnDestroy() {
+    this.notificationsSub.unsubscribe();
+  }
+
+}
