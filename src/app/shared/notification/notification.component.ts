ec7da3c7bb0ccd6b4f9a3acb165869a7a0aa3936
Joe-Sendra <joe.sendra@gmail.com>
2019-12-10 18:23:32 -0600

    changed input from array to single object

--- src/app/shared/notification/notification.component.ts
+++ src/app/shared/notification/notification.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 
+import { NotificationInfo } from './notification-info.model';
+
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
@@ -7,65 +9,8 @@ import { Component, Input } from '@angular/core';
 })
 export class NotificationComponent {
 
+  @Input() notification: NotificationInfo;
+
   // TODO consider time or expiration, specific audience?, data model
 
-  // TODO get this from an input or service
-  notifications = [
-    {
-      type: 'info',
-      header: 'Exam info',
-      title: 'CS200 exam',
-      message: 'This is a message about some exam, blah blah blah',
-      created: 'Dec 9, 2019'
-    },
-    {
-      type: 'danger',
-      header: 'Campus safety',
-      title: 'Holiday safety tips',
-      message: 'Be careful!',
-      created: 'Dec 19, 2019'
-    },
-    {
-      type: 'danger',
-      header: 'Campus safety',
-      title: 'Holiday safety tips',
-      message: 'Be careful!',
-      created: 'Dec 19, 2019'
-    },
-    {
-      type: 'danger',
-      header: 'Campus safety',
-      title: 'Holiday safety tips',
-      message: 'Be careful!',
-      created: 'Dec 19, 2019'
-    },
-    {
-      type: 'info',
-      header: 'Exam info',
-      title: 'CS200 exam',
-      message: 'This is a message about some exam, blah blah blah',
-      created: 'Dec 9, 2019'
-    },
-    {
-      type: 'danger',
-      header: 'Campus safety',
-      title: 'Holiday safety tips',
-      message: 'Be careful!',
-      created: 'Dec 19, 2019'
-    },
-    {
-      type: 'info',
-      header: 'Campus safety',
-      title: 'Holiday safety tips',
-      message: 'Be careful!',
-      created: 'Dec 19, 2019'
-    },
-    {
-      type: 'info',
-      header: 'Campus safety',
-      title: 'Holiday safety tips',
-      message: 'Be careful!',
-      created: 'Dec 19, 2019'
-    }
-  ];
 }
