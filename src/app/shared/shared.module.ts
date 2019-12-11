ae5c12ca7dd708e2b9ee8cd7e789fa17c39bb8aa
Joe-Sendra <joe.sendra@gmail.com>
2019-12-11 11:17:52 -0600

    added ng-bootstrap

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AuthService } from './auth/auth.service';
 
@@ -20,7 +21,8 @@ import { NotificationEditComponent } from './notification/notification-edit/noti
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgbModule
   ],
   exports: [
     CommonModule,
