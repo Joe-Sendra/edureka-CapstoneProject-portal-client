101dda8990f12c99b452db958c111e37daa7ba13
Joe-Sendra <joe.sendra@gmail.com>
2019-12-12 17:14:06 -0600

    add a user Behavior Subject to service

--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -6,6 +6,7 @@ export class AuthService {
 
   private isLoggedIn = new BehaviorSubject<boolean>(null);
   private role = new BehaviorSubject<string>(null);
+  private user = new BehaviorSubject<any>(null);
 
   constructor() {
     // TODO change this once implementing auto login with valid token
@@ -20,8 +21,17 @@ export class AuthService {
     return this.role;
   }
 
-  updateLoginStatus(status: boolean, role: string) {
+  getUser() {
+    return this.user;
+  }
+
+  updateLoginStatus(status: boolean, role: string, user?: any) {
     this.isLoggedIn.next(status);
+    if (user) {
+      this.user.next(user);
+    } else {
+      this.user.next(null);
+    }
     this.role.next(role);
   }
 }
